d1640f5eae737a930cceafd0c2d2216abd12701c
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2023-07-21 14:00:59 +0200

    maquetación home

--- formulary/src/components/Home/Home.jsx
+++ formulary/src/components/Home/Home.jsx
@@ -5,7 +5,15 @@ function Home() {
     const [formData, setFormData] = useState('');
     return (
         <div class="home-section">
-            <p>home</p>
+            <h1>Join our Newsletter!</h1>
+            <p>If you're interested in the topics discussed in the article, you're algo going to love our newsletter! Join Now!</p>
+            <div className="home-form">
+                <form>
+                    <input className="home-input"></input>
+                    <button className="home-button">Join</button> 
+                </form>
+                
+            </div>
         </div>
         );
     }
